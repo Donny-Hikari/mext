2f28a03409949b4ec3e59bc07347903838a33048
Donny Hikari <donny.hikari@gmail.com>
2024-01-30 22:55:18 +0900

    Remove unnecessary logging setup code in utils.

--- mext/libs/utils.py
+++ mext/libs/utils.py
@@ -1,27 +1,7 @@
-import logging
 import sys
 from typing import Any
 import asyncio
 import os
-
-LOG_STATS = 11
-LOG_VERBOSE = 15
-
-def setupLogging(logger: logging.Logger, log_level=logging.INFO, format='[%(levelname)-s %(asctime)s]: %(message)s'):
-  logging.addLevelName(LOG_STATS, 'STATS')
-  logging.addLevelName(LOG_VERBOSE, 'VERBOSE')
-
-  herr = logging.StreamHandler(sys.stderr)
-  herr.setLevel(logging.ERROR)
-  herr.setFormatter(logging.Formatter(format))
-  logger.addHandler(herr)
-
-  hout = logging.StreamHandler(sys.stdout)
-  hout.addFilter(lambda record: record.levelno < logging.ERROR)
-  hout.setFormatter(logging.Formatter(format))
-  logger.addHandler(hout)
-
-  logger.setLevel(log_level)
 
 def ensure_folder_exists(fn):
   folder = os.path.dirname(fn)
